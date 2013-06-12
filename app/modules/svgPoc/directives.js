75b416e0c4cd39513ee3165aa1ec18a813f1c425
Jason More <Jason.More@gmail.com>
2013-06-12 15:05:17 -0500

    working on the bounding box

--- app/modules/svgPoc/directives.js
+++ app/modules/svgPoc/directives.js
@@ -110,117 +110,17 @@ angular.module('svgPoc')
   .directive('boundingBox', function (surfaceService) {
     return {
       restrict: 'E',
-      template: '<rect x="{{x}}" y="{{y}}" width="0" height="0" stroke="black" fill="yellow" opacity="0.4"/>',
+      templateUrl: 'modules/svgPoc/partials/boundingBox.html',
+      replace: true,
+//      transclude: true,
       scope: {
-        model: '='
+        asdfasdf: '='
       },
       controller: function($scope, $element, $attrs){
-        if(!$scope.model){
-          return;
-        }
 
-        $scope.x = $scope.model.box.x;
-        $scope.y = $scope.model.box.y;
-        $scope.width = $scope.model.box.width;
-        $scope.height = $scope.model.box.height;
-
-        $scope.$watch($scope.model, function (newVal, oldVal) {
-          if (newVal === oldVal) {
-            return;
-          }
-
-          $element[0].setAttributeNS(null, "transform", "matrix(" + $scope.model.transform + ")");
-        });
       }
-//      link: function ($scope, el, attr) {
-//        $scope.$watch(attr.transform, function (newVal, oldVal) {
-//          if (newVal === oldVal) {
-//            return;
-//          }
-//
-//          el[0].setAttributeNS(null, "transform", "matrix(" + $scope.boundingBoxTransform + ")");
-//        });
-//      }
-
     };
   })
-
-  .directive('hasBoundingBox', function() {
-    return {
-      restrict: 'A',
-      template: 'hihihi'
-    }
-  })
-
-//  .directive('boxable', function (surfaceService) {
-//    return {
-//      restrict: 'A',
-//      link: function ($scope, el, attr) {
-//        var boundingBox;
-//
-//        $scope.$watch(attr.boxable, function (value) {
-//          boundingBox = value;
-//        });
-//
-//        el.bind('mousedown', function (e) {
-//
-//          var transform = el[0].getCTM();
-//          transform.scale = true;
-//          var transformMatrix = transform.a + " " + transform.b + " " + transform.c + " " + transform.d + " " + transform.e + " " + transform.f;
-//
-//          $scope.$apply(function () {
-//            $scope.boundingBox = {
-//              box: el[0].getBBox(),
-//              transform: transformMatrix
-//            };
-//          });
-//        });
-//      }
-//    };
-//  })
-//      link: function ($scope, el, attr) {
-//
-//        el.bind('mousedown', function (e) {
-//
-//          var transform = el[0].getCTM();
-//          transform.scale = true;
-//          var transformMatrix = transform.a + " " + transform.b + " " + transform.c + " " + transform.d + " " + transform.e + " " + transform.f;
-//
-//          attr.boxable = {
-//            box: el[0].getBBox(),
-//            transform: transformMatrix
-//          }
-//        });
-
-//        var boundingBox;
-//        var transform;
-//        var text;
-//        var Ti;
-//
-//        function startup(){
-//          boundingBox = document.getElementById("BB");
-//          text = document.getElementsByTagName("text");
-//          for (var i=0;i<text.length;i++){
-//            Ti = text.item(i);
-//            Ti.setAttribute("onmouseover", "draw(evt)");
-//            Ti.setAttribute("onmousedown", "snug(evt)");
-//          }
-//        }
-//        function draw(evt,s){
-//          var O = evt.target;
-//          var Box = O.getBBox();
-//          boundingBox.setAttributeNS(null, "x", Box.x);
-//          boundingBox.setAttributeNS(null, "y", Box.y);
-//          boundingBox.setAttributeNS(null, "width", Box.width);
-//          boundingBox.setAttributeNS(null, "height", Box.height);
-//          transform=O.getCTM()
-//        }
-//        function snug(evt) {
-//          transform.scale = true;
-//          var s = transform.a + " " + transform.b + " " + transform.c + " " + transform.d + " " + transform.e + " " + transform.f;
-//          boundingBox.setAttributeNS(null, "transform", "matrix(" + s + ")");
-//        }
-
 
 
 
