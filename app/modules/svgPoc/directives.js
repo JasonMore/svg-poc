a55b6b7d15d69ec933c9c23505ca9e115a73d5c4
Jason More <Jason.More@gmail.com>
2013-06-13 14:18:07 -0500

    moved into self eval function

--- app/modules/svgPoc/directives.js
+++ app/modules/svgPoc/directives.js
@@ -1,225 +1,206 @@
-'use strict';
-angular.module('svgPoc')
+(function() {
+  'use strict';
 
-  .directive('surface', function (surfaceService) {
-    return{
-      restrict: 'A',
-      link: function ($scope, el, attr) {
-        surfaceService.element = el;
-      }
-    };
-  })
+  angular.module('svgPoc')
 
-  .directive('drag', function (surfaceService, $timeout) {
-    return {
-      restrict: 'A',
-      link: function ($scope, el, attr) {
-        var box,
-          originalX,
-          originalY,
-          shapeOffsetX,
-          shapeOffsetY;
-
-        $scope.$watch(attr.drag, function (value) {
-          box = value;
-        });
-
-        el.bind('mousedown', function (e) {
-          surfaceService.element.bind('mousemove', drag);
-          surfaceService.element.bind('mouseup', dragDone);
-        });
-
-        function drag(e) {
-          if (!originalX || !originalY) {
-            // start of drag
-            originalX = e.offsetX;
-            originalY = e.offsetY;
-            shapeOffsetX = originalX - box.x;
-            shapeOffsetY = originalY - box.y;
-          }
-
-          var x = e.offsetX - originalX,
-            y = e.offsetY - originalY;
-
-          $scope.$apply(function () {
-            box.shadow = {x: x, y: y};
-          });
+    .directive('surface', function (surfaceService) {
+      return{
+        restrict: 'A',
+        link: function ($scope, el, attr) {
+          surfaceService.element = el;
         }
+      };
+    })
 
-        function dragDone(e) {
-          surfaceService.element.unbind('mousemove', drag);
-          surfaceService.element.unbind('mouseup', dragDone);
+    .directive('drag', function (surfaceService, svgService) {
+      return {
+        restrict: 'A',
+        link: function ($scope, el, attr) {
+          var box,
+            originalX,
+            originalY,
+            shapeOffsetX,
+            shapeOffsetY;
 
-          var isMouseClick = isJustMouseClick(e);
+          $scope.$watch(attr.drag, function (value) {
+            box = value;
+          });
 
-          originalX = null
-          originalY = null;
+          el.bind('mousedown', function (e) {
+            surfaceService.element.bind('mousemove', drag);
+            surfaceService.element.bind('mouseup', dragDone);
+          });
 
-          //debugger;
-          var x = e.offsetX - shapeOffsetX,
-            y = e.offsetY - shapeOffsetY;
-
-
-          $scope.$apply(function () {
-
-            box.shadow = null;
-
-            if (!isMouseClick) {
-              box.x = x;
-              box.y = y;
+          function drag(e) {
+            if (!originalX || !originalY) {
+              // start of drag
+              originalX = e.offsetX;
+              originalY = e.offsetY;
+              shapeOffsetX = originalX - box.x;
+              shapeOffsetY = originalY - box.y;
             }
 
-            // hack :-(
-            // need to calculate transformation after box moved
-            $timeout(function () {
-              var transform = el[0].getCTM();
-              transform.scale = true;
-              var transformMatrix = transform.a + " " + transform.b + " " + transform.c + " " + transform.d + " " + transform.e + " " + transform.f;
+            var x = e.offsetX - originalX,
+              y = e.offsetY - originalY;
 
-              box.boundingBox = {
-                box: el[0].getBBox(),
-                transform: transformMatrix
-              };
-            }, 0);
-          });
-        }
-
-        function isJustMouseClick(e) {
-          if (!originalX || !originalY) {
-            return true;
+            $scope.$apply(function () {
+              box.shadow = {x: x, y: y};
+            });
           }
 
-          var xMovement = originalX - e.offsetX;
-          var xMovedLittle = xMovement < 3 && xMovement > -3;
+          function dragDone(e) {
+            surfaceService.element.unbind('mousemove', drag);
+            surfaceService.element.unbind('mouseup', dragDone);
 
-          var yMovement = originalY - e.offsetY;
-          var yMovedLittle = yMovement < 3 && yMovement > -3;
+            var isMouseClick = isJustMouseClick(e);
 
-          if (xMovedLittle && yMovedLittle) {
-            // just a mouse click
-            return true;
+            originalX = null
+            originalY = null;
+
+            //debugger;
+            var x = e.offsetX - shapeOffsetX,
+              y = e.offsetY - shapeOffsetY;
+
+
+            $scope.$apply(function () {
+
+              box.shadow = null;
+
+              if (!isMouseClick) {
+                box.x = x;
+                box.y = y;
+              }
+
+              svgService.recalculateTransformation(el[0], box);
+            });
           }
 
-          return false;
-        }
-      }
-    };
-  })
-
-  .directive('resize', function () {
-    return {
-      restrict: 'A',
-      link: function ($scope, el, attr) {
-        var box,
-          originalX,
-          originalY,
-          shapeOffsetX,
-          shapeOffsetY;
-
-        $scope.$watch(attr.drag, function (value) {
-          box = value;
-        });
-
-        el.bind('mousedown', function (e) {
-          surfaceService.element.bind('mousemove', drag);
-          surfaceService.element.bind('mouseup', dragDone);
-        });
-
-        function drag(e) {
-          if (!originalX || !originalY) {
-            // start of drag
-            originalX = e.offsetX;
-            originalY = e.offsetY;
-            shapeOffsetX = originalX - box.x;
-            shapeOffsetY = originalY - box.y;
-          }
-
-          var x = e.offsetX - originalX,
-            y = e.offsetY - originalY;
-
-          $scope.$apply(function () {
-            box.shadow = {x: x, y: y};
-          });
-        }
-
-        function dragDone(e) {
-          surfaceService.element.unbind('mousemove', drag);
-          surfaceService.element.unbind('mouseup', dragDone);
-
-          var isMouseClick = isJustMouseClick(e);
-
-          originalX = null
-          originalY = null;
-
-          //debugger;
-          var x = e.offsetX - shapeOffsetX,
-            y = e.offsetY - shapeOffsetY;
-
-
-          $scope.$apply(function () {
-
-            box.shadow = null;
-
-            if (!isMouseClick) {
-              box.x = x;
-              box.y = y;
+          function isJustMouseClick(e) {
+            if (!originalX || !originalY) {
+              return true;
             }
 
-            // hack :-(
-            // need to calculate transformation after box moved
-            $timeout(function () {
-              var transform = el[0].getCTM();
-              transform.scale = true;
-              var transformMatrix = transform.a + " " + transform.b + " " + transform.c + " " + transform.d + " " + transform.e + " " + transform.f;
+            var xMovement = originalX - e.offsetX;
+            var xMovedLittle = xMovement < 3 && xMovement > -3;
 
-              box.boundingBox = {
-                box: el[0].getBBox(),
-                transform: transformMatrix
-              };
-            }, 0);
+            var yMovement = originalY - e.offsetY;
+            var yMovedLittle = yMovement < 3 && yMovement > -3;
+
+            if (xMovedLittle && yMovedLittle) {
+              // just a mouse click
+              return true;
+            }
+
+            return false;
+          }
+        }
+      };
+    })
+
+    .directive('resize', function (svgService) {
+      return {
+        restrict: 'A',
+        link: function ($scope, el, attr) {
+          var box,
+            originalX,
+            originalY,
+            shapeOffsetX,
+            shapeOffsetY;
+
+          $scope.$watch(attr.drag, function (value) {
+            box = value;
           });
-        }
 
-        function isJustMouseClick(e) {
-          if (!originalX || !originalY) {
-            return true;
+          el.bind('mousedown', function (e) {
+            surfaceService.element.bind('mousemove', drag);
+            surfaceService.element.bind('mouseup', dragDone);
+          });
+
+          function drag(e) {
+            if (!originalX || !originalY) {
+              // start of drag
+              originalX = e.offsetX;
+              originalY = e.offsetY;
+              shapeOffsetX = originalX - box.x;
+              shapeOffsetY = originalY - box.y;
+            }
+
+            var x = e.offsetX - originalX,
+              y = e.offsetY - originalY;
+
+            $scope.$apply(function () {
+              box.shadow = {x: x, y: y};
+            });
           }
 
-          var xMovement = originalX - e.offsetX;
-          var xMovedLittle = xMovement < 3 && xMovement > -3;
+          function dragDone(e) {
+            surfaceService.element.unbind('mousemove', drag);
+            surfaceService.element.unbind('mouseup', dragDone);
 
-          var yMovement = originalY - e.offsetY;
-          var yMovedLittle = yMovement < 3 && yMovement > -3;
+            var isMouseClick = isJustMouseClick(e);
 
-          if (xMovedLittle && yMovedLittle) {
-            // just a mouse click
-            return true;
+            originalX = null
+            originalY = null;
+
+            //debugger;
+            var x = e.offsetX - shapeOffsetX,
+              y = e.offsetY - shapeOffsetY;
+
+
+            $scope.$apply(function () {
+
+              box.shadow = null;
+
+              if (!isMouseClick) {
+                box.x = x;
+                box.y = y;
+              }
+
+
+              svgService.recalculateTransformation(el[0], box);
+            });
           }
 
-          return false;
+          function isJustMouseClick(e) {
+            if (!originalX || !originalY) {
+              return true;
+            }
+
+            var xMovement = originalX - e.offsetX;
+            var xMovedLittle = xMovement < 3 && xMovement > -3;
+
+            var yMovement = originalY - e.offsetY;
+            var yMovedLittle = yMovement < 3 && yMovement > -3;
+
+            if (xMovedLittle && yMovedLittle) {
+              // just a mouse click
+              return true;
+            }
+
+            return false;
+          }
         }
+
       }
+    })
 
-    }
-  })
+    .directive('boundingBox', function (svgService) {
+      return {
+        restrict: 'E',
+        templateUrl: 'modules/svgPoc/partials/boundingBox.html',
+        replace: true,
+        compile: svgService.compile
+      };
+    })
 
-  .directive('boundingBox', function (svgService) {
-    return {
-      restrict: 'E',
-      templateUrl: 'modules/svgPoc/partials/boundingBox.html',
-      replace: true,
-      compile: svgService.compile
-    };
-  })
+    .directive('corner', function (svgService) {
+      return {
+        restrict: 'E',
+        replace: true,
+        template: '<rect class="nwse-resize" stroke="white" fill="blue" width="6" height="6" ng-attr-x="{{activeBox.boundingBox.box.x - 6 }}" ng-attr-y="{{activeBox.boundingBox.box.y - 6}}"></rect>',
+        compile: svgService.compile
+      }
+    })
 
-  .directive('corner', function (svgService) {
-    return {
-      restrict: 'E',
-      replace: true,
-      template: '<rect class="nwse-resize" stroke="white" fill="blue" width="6" height="6" ng-attr-x="{{activeBox.boundingBox.box.x - 6 }}" ng-attr-y="{{activeBox.boundingBox.box.y - 6}}"></rect>',
-      compile: svgService.compile
-    }
-  })
-
-
-;
+  ;
+}());
--- app/modules/svgPoc/services.js
+++ app/modules/svgPoc/services.js
@@ -1,44 +1,63 @@
-'use strict';
-angular.module('svgPoc')
+(function() {
+  'use strict';
+  angular.module('svgPoc')
 
-  .service('surfaceService', function() {
-    this.element;
-    this.transform;
-  })
+    .service('surfaceService', function() {
+      this.element;
+      this.transform;
+    })
 
-  // this is broken for now
-  .service('svgService', function(){
+    // this is broken for now
+    .service('svgService', function($timeout){
 
-    var xmlns = "http://www.w3.org/2000/svg";
-    var compileNode = function(angularElement){
-      var rawElement = angularElement[0];
+      this.recalculateTransformation = function(el, box) {
+        // hack :-(
+        // need to calculate transformation after box moved
+        $timeout(function () {
+          var transform = el.getCTM();
+          transform.scale = true;
+          var transformMatrix = transform.a + " " + transform.b + " " + transform.c + " " + transform.d + " " + transform.e + " " + transform.f;
 
-      if(!rawElement.localName){
-        return document.createTextNode(rawElement.wholeText);
-      }
-      var replacement = document.createElementNS(xmlns,rawElement.localName);
-      var children = angularElement.children();
-      
-      angular.forEach(children, function (value) {
-        var newChildNode = compileNode(angular.element(value));
-        replacement.appendChild(newChildNode[0]);
-      });
-
-      var attributes = rawElement.attributes;
-      for (var i = 0; i < attributes.length ; i++) {
-        replacement.setAttribute(attributes[i].name, attributes[i].value);
-      }
-      angularElement = angularElement.replaceWith(replacement);
-
-      return angularElement;
-    };
-
-    this.compile = function(elem, attrs, transclude) {
-      compileNode(elem);
-
-      return function postLink(scope, elem,attrs,controller){
-//        console.log('link called');
+          box.boundingBox = {
+            box: el.getBBox(),
+            transform: transformMatrix
+          };
+        }, 0);
       };
-    }
-  })
-;
+
+
+      var xmlns = "http://www.w3.org/2000/svg";
+      var compileNode = function(angularElement){
+        var rawElement = angularElement[0];
+
+        if(!rawElement.localName){
+          return document.createTextNode(rawElement.wholeText);
+        }
+        var replacement = document.createElementNS(xmlns,rawElement.localName);
+        var children = angularElement.children();
+
+        angular.forEach(children, function (value) {
+          var newChildNode = compileNode(angular.element(value));
+          replacement.appendChild(newChildNode[0]);
+        });
+
+        var attributes = rawElement.attributes;
+        for (var i = 0; i < attributes.length ; i++) {
+          replacement.setAttribute(attributes[i].name, attributes[i].value);
+        }
+        angularElement = angularElement.replaceWith(replacement);
+
+        return angularElement;
+      };
+
+      this.compile = function(elem, attrs, transclude) {
+        compileNode(elem);
+
+        return function postLink(scope, elem,attrs,controller){
+//        console.log('link called');
+        };
+      }
+    })
+  ;
+}());
+
