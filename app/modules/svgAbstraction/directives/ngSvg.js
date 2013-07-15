adadf3fae36626127d9e7aa1d4e3e91798dad450
Jason More <Jason.More@gmail.com>
2013-07-15 14:51:12 -0500

    nv svg tests

--- app/modules/svgAbstraction/directives/ngSvg.js
+++ app/modules/svgAbstraction/directives/ngSvg.js
@@ -1,20 +1,22 @@
 (function () {
   angular.module('svgAbstraction.directives')
-    .directive('ngSvg', function ($compile) {
+    .directive('ngSvg', function () {
       return {
         restrict: 'E',
         scope: true,
         transclude: true,
         replace: true,
         template: '<div ng-transclude></div>',
-        controller: function ($scope, $element) {
-          var self = this;
-
-          // yes, belongs in link, but easier here
-          $element.svg({onLoad: function (svg) {
-            self.svg = svg;
-          }});
-        }
+        controller: 'ngSvgController'
       };
-    });
+    })
+    .controller('ngSvgController', function($element) {
+      var self = this;
+
+      // yes, belongs in link, but easier here
+      $element.svg({onLoad: function (svg) {
+        self.svg = svg;
+      }});
+    })
+  ;
 })();
--- test/unit/svgAbstraction/directives/ngSvgSpec.js
+++ test/unit/svgAbstraction/directives/ngSvgSpec.js
@@ -1,19 +1,24 @@
 describe('ngSvg', function() {
   var element,
-    scope;
+    scope,
+    controller;
 
   beforeEach(module('svgAbstraction'));
 
-  beforeEach(inject(function($rootScope, $compile){
+  beforeEach(inject(function($rootScope, $compile, $controller){
     element = angular.element('<ng-svg></ng-svg>');
 
     scope = $rootScope;
+    controller = $controller('ngSvgController', {$element: element});
     $compile(element)(scope);
-    scope.$digest();
   }));
 
   it('creates real svg element', function() {
     var svg = element.find('svg');
     expect(svg.length).toBe(1);
+  });
+
+  it('sets jquery.svg element on controller', function() {
+    expect(controller.svg).toBeDefined();
   });
 });
