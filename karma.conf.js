db04e3747bc5fc060422089d86a65700c17eeeaf
Jason More <Jason.More@gmail.com>
2013-07-16 10:09:00 -0500

    added new karma config. Added unit tests for shape rendering

--- karma.conf.js
+++ karma.conf.js
@@ -1,97 +1,96 @@
 // Karma configuration
-// Generated on Fri Jun 07 2013 12:01:03 GMT-0500 (Central Daylight Time)
+// Generated on Tue Jul 16 2013 08:25:24 GMT-0500 (CDT)
+
+module.exports = function(karma) {
+  karma.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// frameworks to use
-frameworks = ['jasmine'];
+//    preprocessors: {
+//      'test/unit/**/*.html': ['ng-html2js']
+//    },
 
-preprocessors = {
-  'test/unit/**/*.html': 'html2js'
+//    plugins: ['ng-html2js'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // -- add libraries here as they are added to index.html
+      'app/components/jquery/jquery.js',
+      'app/components/jquery-ui/ui/jquery-ui.js',
+      'app/components/bootstrap-css/js/bootstrap.js',
+      'app/components/jquery.svg/jquery.svg.js',
+      'app/components/lodash/dist/lodash.js',
+      'app/components/angularjs-bower/angular.js',
+      'app/components/angularjs-bower/angular-resource.js',
+      'app/components/angularjs-bower/angular-cookies.js',
+      'app/components/angularjs-bower/angular-sanitize.js',
+      'app/components/angular-ui-router/release/angular-ui-router.js',
+      'app/components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'app/components/svg.js/dist/svg.js',
+      'app/components/svg.draggable.js/svg.draggable.js',
+      // end of libraries
+
+      'app/components/angularjs-bower/angular-mocks.js',
+      'app/modules/**/*.js',
+      'test/unit/**/*.js',
+//      'test/unit/**/*.html'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // cli runner port
+    runnerPort: 9100,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+    logLevel: karma.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// list of files / patterns to load in the browser
-files = [
-
-  // -- add libraries here as they are added to index.html
-  'app/components/jquery/jquery.js',
-  'app/components/jquery-ui/ui/jquery-ui.js',
-  'app/components/bootstrap-css/js/bootstrap.js',
-  'app/components/jquery.svg/jquery.svg.js',
-  'app/components/lodash/dist/lodash.js',
-  'app/components/angularjs-bower/angular.js',
-  'app/components/angularjs-bower/angular-resource.js',
-  'app/components/angularjs-bower/angular-cookies.js',
-  'app/components/angularjs-bower/angular-sanitize.js',
-  'app/components/angular-ui-router/release/angular-ui-router.js',
-  'app/components/angular-bootstrap/ui-bootstrap-tpls.js',
-  'app/components/svg.js/dist/svg.js',
-  'app/components/svg.draggable.js/svg.draggable.js',
-  // end of libraries
-
-  'app/components/angularjs-bower/angular-mocks.js',
-  'app/modules/**/*.js',
-  'test/unit/**/*.js',
-  'test/unit/**/*.html'
-];
-
-
-// list of files to exclude
-exclude = [
-  'app/components/angularjs-bower/*.min.js'
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-reporters = ['progress'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
-
-
-// plugins to load
-plugins = [
-  'karma-jasmine',
-  'karma-chrome-launcher'
-];
--- test/unit/svgAbstraction/directives/shapeSpec/shapeSpec.js
+++ test/unit/svgAbstraction/directives/shapeSpec/shapeSpec.js
@@ -1,16 +1,163 @@
-describe('shape', function() {
-  var element,
-    scope,
-    controller;
+describe('shape', function () {
+  var htmlToRender,
+    act,
+    element,
+    scope;
 
   beforeEach(module('svgAbstraction'));
-  beforeEach(inject(function ($rootScope, $compile, $templateCache, $controller) {
-    $templateCache.get('oneShape.html')
-
-    element = angular.element();
-    scope = $rootScope;
-    controller = $controller('ngSvgController', {$element: element});
-
-    $compile(element)(scope);
+  beforeEach(inject(function ($rootScope, $compile, $controller) {
+    act = function () {
+      element = angular.element(htmlToRender);
+      scope = $rootScope;
+      $compile(element)(scope);
+    };
   }));
+
+  describe('renders', function () {
+    var parentGroup,
+      shape,
+      text;
+
+    describe('array of shapes', function () {
+      beforeEach(function () {
+        htmlToRender =
+          '<ng-svg style="height: 600px">' +
+            '<shape top="shape.top"' +
+            'left="shape.left"' +
+            'd="shape.path"' +
+            'fill="shape.backgroundColor"' +
+            'stroke="shape.borderColor"' +
+            'stroke-width="shape.borderWidth"' +
+            'ng-repeat="shape in shapes"' +
+            '></shape>' +
+            '</ng-svg>'
+
+        act();
+
+        scope.shapes = [
+          {
+            top:50,
+            left:50,
+            path:'M0,0L100,0L100,100L0,100z',
+            backgroundColor:'green',
+            borderColor:'blue',
+            borderWidth:12
+          },
+          {
+            top:100,
+            left:100,
+            path:'M0,0L100,0L100,100L0,100z',
+            backgroundColor:'gray',
+            borderColor:'black',
+            borderWidth:'2'
+          }
+        ];
+
+        scope.$digest();
+        parentGroup = element.find('g:last');
+        shape = parentGroup.find('path');
+        text = parentGroup.find('text');
+      });
+
+      it('is on screen', function () {
+        expect(parentGroup.length).toBe(1);
+      });
+
+      it('parent group has correct transformation', function () {
+        expect(parentGroup.attr('transform')).toEqual('translate(100,100), rotate(0,50,50)');
+      });
+
+      it('parent group has path', function () {
+        expect(shape.length).toBe(1);
+      });
+
+      it('shape has correct path', function () {
+        expect(shape.attr('d')).toEqual('M0,0L100,0L100,100L0,100z');
+      });
+
+      it('shape has background color green', function () {
+        expect(shape.attr('fill')).toEqual('gray');
+      });
+
+      it('shape has border color blue', function () {
+        expect(shape.attr('stroke')).toEqual('black');
+      });
+
+      it('shape has border width of 12', function () {
+        expect(shape.attr('stroke-width')).toEqual('2');
+      });
+
+      describe('removing shape', function () {
+        beforeEach(function () {
+          scope.shapes.splice(scope.shapes.length - 1, 1);
+          scope.$digest();
+        });
+
+        it('reduces shape count from 2 to 1', function () {
+          expect(element.find('g').length).toBe(1);
+        });
+      });
+    });
+
+    describe('single shape', function () {
+      beforeEach(function () {
+        htmlToRender =
+          '<ng-svg style="height: 600px">' +
+            '<shape top="shape.top"' +
+            'left="shape.left"' +
+            'd="shape.path"' +
+            'fill="shape.backgroundColor"' +
+            'stroke="shape.borderColor"' +
+            'stroke-width="shape.borderWidth"' +
+            '></shape>' +
+            '</ng-svg>'
+
+        act();
+
+        scope.shape = {
+          top:50,
+          left:50,
+          path:'M0,0L100,0L100,100L0,100z',
+          backgroundColor:'green',
+          borderColor:'blue',
+          borderWidth:12
+        };
+
+        scope.$digest();
+        parentGroup = element.find('g');
+        shape = parentGroup.find('path');
+        text = parentGroup.find('text');
+      });
+
+      it('is on screen', function () {
+        expect(parentGroup.length).toBe(1);
+      });
+
+      it('parent group has correct transformation', function () {
+        expect(parentGroup.attr('transform')).toEqual('translate(50,50), rotate(0,50,50)');
+      });
+
+      it('parent group has path', function () {
+        expect(shape.length).toBe(1);
+      });
+
+      it('shape has correct path', function () {
+        expect(shape.attr('d')).toEqual('M0,0L100,0L100,100L0,100z');
+      });
+
+      it('shape has background color green', function () {
+        expect(shape.attr('fill')).toEqual('green');
+      });
+
+      it('shape has border color blue', function () {
+        expect(shape.attr('stroke')).toEqual('blue');
+      });
+
+      it('shape has border width of 12', function () {
+        expect(shape.attr('stroke-width')).toEqual('12');
+      });
+    });
+  });
+
+
 });
