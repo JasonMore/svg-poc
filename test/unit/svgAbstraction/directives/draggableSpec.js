bcdbe8b0f51b027cb3f17569f73aecccc994f646
Jason More <Jason.More@gmail.com>
2013-07-17 16:47:43 -0500

    added draggable tests

--- test/unit/svgAbstraction/directives/draggableSpec.js
+++ test/unit/svgAbstraction/directives/draggableSpec.js
@@ -18,15 +18,15 @@ describe('draggable', function() {
     beforeEach(function () {
       htmlToRender =
         '<ng-svg style="height: 600px">' +
-        ' <shape top="shape.top"' +
-        '  left="shape.left"' +
-        '  d="shape.path"' +
-        '  fill="shape.backgroundColor"' +
-        '  stroke="shape.borderColor"' +
-        '  stroke-width="shape.borderWidth"' +
-        '  draggable = "true"' +
-        ' ></shape>' +
-        '</ng-svg>';
+          ' <shape top="shape.top"' +
+          '  left="shape.left"' +
+          '  d="shape.path"' +
+          '  fill="shape.backgroundColor"' +
+          '  stroke="shape.borderColor"' +
+          '  stroke-width="shape.borderWidth"' +
+          '  draggable = "true"' +
+          ' ></shape>' +
+          '</ng-svg>';
 
       act();
 
@@ -62,20 +62,73 @@ describe('draggable', function() {
 
       $(document).trigger(mousemove);
       $(document).trigger(mouseup);
+      $(document).trigger($.Event("mouseup"));
     });
 
-    it('moves shape', function() {
+    it('moves shape top', function() {
       expect(scope.shape.top).toEqual(10);
+    });
+
+    it('moves shape left', function() {
       expect(scope.shape.left).toEqual(10);
     });
-
-    // TODO: have it work with more than one it
-//    it('moves shape left', function() {
-//      expect(scope.shape.left).toEqual(10);
-//    });
   });
 
-  describe('when shape is not draggable', function() {
+  beforeEach(function () {
+    htmlToRender =
+      '<ng-svg style="height: 600px">' +
+        ' <shape top="shape.top"' +
+        '  left="shape.left"' +
+        '  d="shape.path"' +
+        '  fill="shape.backgroundColor"' +
+        '  stroke="shape.borderColor"' +
+        '  stroke-width="shape.borderWidth"' +
+        '  draggable = "false"' +
+        ' ></shape>' +
+        '</ng-svg>';
 
+    act();
+
+    scope.shape = {
+      top:0,
+      left:0,
+      path:'M0,0L100,0L100,100L0,100z',
+      backgroundColor:'green',
+      borderColor:'blue',
+      borderWidth:12
+    };
+
+    scope.$digest();
+
+    var mouseDown = $.Event('mousedown', {
+      which: 1,
+      pageX: 50,
+      pageY: 50
+    });
+
+    var mousemove = $.Event("mousemove.draggable", {
+      pageX: 60,
+      pageY: 60
+    });
+
+    var mouseup = $.Event("mouseup.draggable", {
+      pageX: 60,
+      pageY: 60
+    });
+
+    var parentGroup = element.find('g');
+    parentGroup.trigger(mouseDown);
+
+    $(document).trigger(mousemove);
+    $(document).trigger(mouseup);
+    $(document).trigger($.Event("mouseup"));
+  });
+
+  it('moves shape top', function() {
+    expect(scope.shape.top).toEqual(0);
+  });
+
+  it('moves shape left', function() {
+    expect(scope.shape.left).toEqual(0);
   });
 });
