194729e8e45bcddb1e8382ee0048eba3585249e6
Jason More <Jason.More@gmail.com>
2013-08-07 11:14:50 -0500

    added coverage around resizing heart.

--- test/unit/svgAbstraction/directives/selectionBoxSpec.js
+++ test/unit/svgAbstraction/directives/selectionBoxSpec.js
@@ -90,7 +90,7 @@ describe('selectionBoxSpec.js', function () {
     });
   });
 
-  describe('when resizing', function() {
+  describe('when resizing square', function() {
     var act,
       corner,
       move;
@@ -587,18 +587,125 @@ describe('selectionBoxSpec.js', function () {
             expect(scope.shapes[0].midPointY).toEqual(52.5355339050293);
           });
 
-//          it('shape rotation stays the same', function() {
-//            expect(scope.shapes[0].rotation).toEqual(0);
-//          });
-//
-//          it('increases shape height and width by 10', function() {
-//            expect(scope.shapes[0].path).toEqual('M0,0L110.204,0L110.204,110.204L0,110.204z');
-//          });
-
         });
       });
 
     });
+
+    describe('when resizing heart', function() {
+      beforeEach(function() {
+        scope.shapes[0].path = 'M190.95184190571857,18.67034584574202c-22.323836384105082,-25.959966259055825 -59.94907374597721,-24.70023237340574 -83.60038330843584,2.7667085108788574L103.48050782485348,25.934428571291733l-3.870951376425747,-4.497373337497029c-23.651309562458632,-27.466939656241248 -61.27654692433076,-28.726676348847562 -83.60038330843584,-2.7667085108788574c-22.333969029440297,25.936421510213755 -21.2598313123552,69.65041398214339 2.381344245776086,97.11735083142841l85.07985583076164,98.8480189913071l85.07985583076164,-98.8480189913071C212.20154057273854,88.32075982788542 213.28581576713086,44.60676174204332 190.95184190571857,18.67034584574202z',
+
+        scope.$digest();
+      });
+
+      describe('nw corner', function() {
+        beforeEach(function () {
+          corner = selectionBoxGroup.find('#cornerNW');
+        });
+        describe('down and right', function() {
+          beforeEach(function () {
+            move = { x: 10, y: 10 };
+            act();
+          });
+
+          it('decreases shape height and width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M171.467,16.765c-20.046,-23.311 -53.832,-22.18 -75.07,2.484L92.921,23.288l-3.476,-4.038c-21.238,-24.664 -55.024,-25.795 -75.07,-2.484c-20.055,23.29 -19.09,62.543 2.138,87.207l76.398,88.761l76.398,-88.761C190.548,79.308 191.522,40.055 171.467,16.765z');
+          });
+        });
+        describe('up and left', function() {
+          beforeEach(function () {
+            move = { x: -10, y: -10 };
+            act();
+          });
+
+          it('increases shape height and width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M210.437,20.575c-24.602,-28.609 -66.066,-27.221 -92.131,3.049L114.04,28.581l-4.266,-4.956c-26.065,-30.27 -67.529,-31.658 -92.131,-3.049c-24.613,28.583 -23.429,76.758 2.624,107.027l93.761,108.935l93.761,-108.935C233.855,97.333 235.05,49.158 210.437,20.575z');
+          });
+        });
+      });
+
+      describe('ne corner', function() {
+        beforeEach(function () {
+          corner = selectionBoxGroup.find('#cornerNE');
+        });
+        describe('down and right', function() {
+          beforeEach(function () {
+            move = { x: 10, y: 10 };
+            act();
+          });
+
+          it('decreases shape height and increases width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M210.437,16.765c-24.602,-23.311 -66.066,-22.18 -92.131,2.484L114.04,23.288l-4.266,-4.038c-26.065,-24.664 -67.529,-25.795 -92.131,-2.484c-24.613,23.29 -23.429,62.543 2.624,87.207l93.761,88.761l93.761,-88.761C233.855,79.308 235.05,40.055 210.437,16.765z');
+          });
+
+        });
+        describe('up and left', function() {
+          beforeEach(function () {
+            move = { x: -10, y: -10 };
+            act();
+          });
+
+          it('increases height and decreases width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M171.467,20.575c-20.046,-28.609 -53.832,-27.221 -75.07,3.049L92.921,28.581l-3.476,-4.956c-21.238,-30.27 -55.024,-31.658 -75.07,-3.049c-20.055,28.583 -19.09,76.758 2.138,107.027l76.398,108.935l76.398,-108.935C190.548,97.333 191.522,49.158 171.467,20.575z');
+          });
+        });
+      });
+
+      describe('se corner', function() {
+        beforeEach(function () {
+          corner = selectionBoxGroup.find('#cornerSE');
+        });
+        describe('down and right', function() {
+          beforeEach(function () {
+            move = { x: 10, y: 10 };
+            act();
+          });
+
+          it('increases shape height and width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M210.437,20.575c-24.602,-28.609 -66.066,-27.221 -92.131,3.049L114.04,28.581l-4.266,-4.956c-26.065,-30.27 -67.529,-31.658 -92.131,-3.049c-24.613,28.583 -23.429,76.758 2.624,107.027l93.761,108.935l93.761,-108.935C233.855,97.333 235.05,49.158 210.437,20.575z');
+          });
+
+        });
+        describe('up and left', function() {
+          beforeEach(function () {
+            move = { x: -10, y: -10 };
+            act();
+          });
+
+          it('decreases shape height and width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M171.467,16.765c-20.046,-23.311 -53.832,-22.18 -75.07,2.484L92.921,23.288l-3.476,-4.038c-21.238,-24.664 -55.024,-25.795 -75.07,-2.484c-20.055,23.29 -19.09,62.543 2.138,87.207l76.398,88.761l76.398,-88.761C190.548,79.308 191.522,40.055 171.467,16.765z');
+          });
+        });
+      });
+
+      describe('sw corner', function() {
+        beforeEach(function () {
+          corner = selectionBoxGroup.find('#cornerSW');
+        });
+        describe('down and right', function() {
+          beforeEach(function () {
+            move = { x: 10, y: 10 };
+            act();
+          });
+
+          it('increases shape height and decreases width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M171.467,20.575c-20.046,-28.609 -53.832,-27.221 -75.07,3.049L92.921,28.581l-3.476,-4.956c-21.238,-30.27 -55.024,-31.658 -75.07,-3.049c-20.055,28.583 -19.09,76.758 2.138,107.027l76.398,108.935l76.398,-108.935C190.548,97.333 191.522,49.158 171.467,20.575z');
+          });
+        });
+        describe('up and left', function() {
+          beforeEach(function () {
+            move = { x: -10, y: -10 };
+            act();
+          });
+
+          it('decreases shape height and increases width by 10', function() {
+            expect(scope.shapes[0].path).toEqual('M210.437,16.765c-24.602,-23.311 -66.066,-22.18 -92.131,2.484L114.04,23.288l-4.266,-4.038c-26.065,-24.664 -67.529,-25.795 -92.131,-2.484c-24.613,23.29 -23.429,62.543 2.624,87.207l93.761,88.761l93.761,-88.761C233.855,79.308 235.05,40.055 210.437,16.765z');
+          });
+        });
+      });
+    });
+
 
   });
 
